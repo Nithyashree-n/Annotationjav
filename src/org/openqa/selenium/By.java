5c74c223070220ec9b932558c8aaffb02910869d
Nithyashree-n <nithyashree.nithyanandam@atmecs.com>
2020-09-22 07:00:34 +0530

    Test testNg

---
+++ src/org/openqa/selenium/By.java
@@ -0,0 +1,15 @@
+package org.openqa.selenium;
+
+public class By {
+
+	public static Object name(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static Object xpath(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/org/openqa/selenium/chrome/ChromeDriver.java
@@ -0,0 +1,25 @@
+package org.openqa.selenium.chrome;
+
+public class ChromeDriver {
+
+	public void get(String url) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Object findElement(Object name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Object manage() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void close() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
